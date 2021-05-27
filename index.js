5e059ed51d95168bee58c5b5aaea6d7807f13c67
Petr Martinek <petet2000@seznam.cz>
2021-05-27 19:49:07 +0200

    Add testing files

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import JavaScriptLexer from './grammars/JavaScriptLexer.js';
 import JavaScriptParser from './grammars/JavaScriptParser.js';
 import testVisitor from './testVisitor.js';
 import {TransformRulesF} from './testVisitor.js';
+import fs from 'fs';
 
-const input = "function Add(a, b) \n { var c = 0; c = a + b; \n return c; \n }" +
-"function Sub(a, b) \n { var c = 0; c = a - b; \n return c; \n }" +
-"function Mult(a, b) \n { var c = 0; c = a * b; \n return c; \n }" +
-"function Div(a, b) \n { var c = 0; c = a / b; \n return c; \n }";
+const input = fs.readFileSync('./test1a').toString()
+
 const chars = new antlr4.InputStream(input);
 const lexer = new JavaScriptLexer(chars);
 const tokens = new antlr4.CommonTokenStream(lexer);
@@ -15,8 +14,10 @@ const parser = new JavaScriptParser(tokens);
 parser.buildParseTrees = true;
 const tree = parser.program();
 
+
 let l = [];
 testVisitor(tree,0,-1,-1,l);
 console.log(l);
 let l2 = TransformRulesF(l);
 console.log(l2);
+
